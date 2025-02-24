919baa1558fe185ffe2898e5a4cb8aa9a1353719
Nelio <nelio.lacerda0433@gmail.com>
2025-02-24 16:14:45 +0000

    feito

---
+++ translations.js
@@ -0,0 +1,54 @@
+const translations = {
+    en: {
+        navBar: {
+            home: "Home",
+            about: "About",
+            qualifications: "Qualifications",
+            skills: "Skills",
+            projects: "Projects",
+            thinkingCorners: "Thinking Corner",
+            contact: "Contact"
+        },
+        title: {
+            p1: "Hello, my name is",
+            p3: "And I'm a ",
+            contact: "Contact"
+        },
+        about: {
+            title: "About me",
+            p11: "I'm Nélio Lacerda and I'm a ",
+            p1: "I’m a Computer Engineering student at Nova SST with" +
+                " a strong passion for logic and mathematics, which naturally " +
+                "fueled my interest in science. I thrive in challenging " +
+                "environments where I can apply and expand my skills, " +
+                "often developing creative, out-of-the-box solutions. " +
+                "As a tech enthusiast, my free time is spent learning " +
+                "new skills through hands-on projects and books.",
+            p21: "Also a thinker",
+            p2: "I’m also a thinker, inspired by the works of Yuval Harari, especially Homo Deus and Sapiens: A Brief\n" +
+                "            History of Humankind. At 15, I began exploring how society and humanity function, which sparked my interest\n" +
+                "            in politics, economics, and social dynamics. This curiosity led to the creation of the ‘Thinking Corner’\n" +
+                "            section in my portfolio, where I share my research on various scientific topics and some of my\n" +
+                "            texts I have written.",
+            donwnload: "Download CV"
+        }
+    },
+    pt: {
+        navBar: {
+            home: "Home",
+            about: "Sobre",
+            qualifications: "Qualificações",
+            skills: "Skills",
+            projects: "Projetos",
+            thinkingCorners: "Thinking Corner",
+            contact: "Contacto"
+        },
+        title: {
+            p1: "Olá, chamo-me",
+            p3: "E eu sou ",
+            contact: "Contact"
+        }
+    }
+}
+
+const langugeSelectd = document.querySelector("select");
